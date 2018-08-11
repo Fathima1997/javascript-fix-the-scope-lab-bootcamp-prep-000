3cd98f79b353401614ca958c31acceaff3583b65
Fathima1997 <fathimareeza9@yahoo.com>
2018-08-11 01:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  var two = n
+ const two = 2
   return n + two
-
   // Feel free to move things around!
-  //const two = 2
+  
 }
 
 var funkyFunction = function() {
